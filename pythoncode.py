fc3cc32cf4b9f2b916dfc3e8614683edc5ae7da6
Md Kutubuddin Sardar <math.mks@gmail.com>
2018-04-24 18:08:01 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -2,4 +2,4 @@ size = 3
 matrix_surprise = [[0] * size] * size
 matrix = [[0]*size for i in range(size)] 
 
-See the image here : List-of-lists-changes-reflected-across-sublists-unexpectedly/uHuck.png
+See the image here : https://github.com/MdKutubuddinSardar/List-of-lists-changes-reflected-across-sublists-unexpectedly/blob/master/uHuck.png
